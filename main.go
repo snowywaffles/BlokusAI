8b4fe49c3488743aaa19c164c4842cd22221db59
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-07-22 18:17:48 -0500

    deleted ebiten. gonna try to just do it in the terminal lol

--- main.go
+++ main.go
@@ -1,39 +1,16 @@
 package main
 
 import (
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"log"
+	"fmt"
 )
 
-type Game struct{}
-
-func (g *Game) Update() error {
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "BlokusAI")
-}
-
-func (g *Game) Layout(outsideWidth int, outsideHeight int) (screenWidth int, screenHeight int) {
-	return 400, 400
-}
-
-var tanSquareImage *ebiten.Image
-
-func initiateImages() {
-	var err error
-	tanSquareImage, _, err = ebitenutil.NewImageFromFile("tan_square.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
 func main() {
-	ebiten.SetWindowSize(800, 800)
-	ebiten.SetWindowTitle("BlokusAI")
-	if err := ebiten.RunGame(&Game{}); err != nil {
-		log.Fatal(err)
+	fmt.Println("hi!")
+	horizontalDisplayLine := " ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----"
+	verticalDisplaySeparator := "|    |    |    |    |    |    |    |    |    |    |    |    |    |    |    |   "
+	for i := 0; i <= 14; i++ {
+		fmt.Println(horizontalDisplayLine)
+		fmt.Println(verticalDisplaySeparator)
 	}
+	fmt.Println(horizontalDisplayLine)
 }
