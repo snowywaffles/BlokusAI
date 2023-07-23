f3309fa1df729f4b831b9807dcb98616238e482e
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-07-22 19:29:54 -0500

    can now get the grid to display values based on the valueInCell map

--- main.go
+++ main.go
@@ -3,21 +3,35 @@ package main
 import (
 	"fmt"
 	"github.com/fatih/color"
+	"log"
 )
 
 func main() {
-	current_number := 1
-	horizontalDisplayLine := ""
+
+	valueInCell := make(map[int]string)
+	valueInCell[1] = "W"
+	valueInCell[10] = "bo"
+	valueInCell[100] = "1111"
+
+	cellNumber := 1
+	horizontalDisplayLine := "---------------------------------------------------------------------------------------------------"
+
+	fmt.Println()
+	fmt.Println(horizontalDisplayLine) // to make the grid look nice
+
 	for i := 0; i <= 13; i++ {
-		verticalDisplaySeparator := "|"
-		horizontalDisplayLine = ""
+		fmt.Printf("|")
 		for i := 0; i <= 13; i++ {
-			verticalDisplaySeparator = verticalDisplaySeparator + " " + (AddLeadingZerosToMakeIntegerThreeDigitsLong(current_number)) + " |"
-			horizontalDisplayLine = horizontalDisplayLine + " -----"
-			current_number++
+			if len(valueInCell[cellNumber]) > 4 {
+				log.Panic("the value in (cell: ", cellNumber, ") is longer than 4 characters")
+			}
+			fmt.Printf(" %-4s |", valueInCell[cellNumber])
+			cellNumber++
 		}
+		fmt.Println()
 		fmt.Println(horizontalDisplayLine)
-		fmt.Println(verticalDisplaySeparator)
 	}
+
 	color.Cyan(horizontalDisplayLine)
+	fmt.Println(valueInCell)
 }
