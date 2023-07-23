ff4a5b1891cc8bc5425b680399f7995400c3010d
Nathan Brown <nathanjeffreybrown@gmail.com>
2023-07-22 19:45:33 -0500

    added a bit of documentation for draw14x14GridWithValues

--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
-	// "github.com/fatih/color"
-	// "log"
+	"github.com/fatih/color"
+	"log"
 )
 
 func draw14x14GridWithValues(valueInCell map[int]string) error {
+
+	// A standard Blokus Duo board is 14x14, or 196 squares.
+	// cellNumber begins at 1, and is incremented every time the next square is made.
+	// TOP LEFT: cellNumber = 1
+	// TOP RIGHT: cellNumber = 14
+	// BOTTOM LEFT: cellNumber = 183
+	// BOTTOM RIGHT: cellNumber = 196
+
 	cellNumber := 1
 	horizontalDisplayLine := "---------------------------------------------------------------------------------------------------"
 
 	fmt.Println()
-	fmt.Println(horizontalDisplayLine) // to make the grid look nice (formatting purposes)
+	fmt.Println(horizontalDisplayLine) // to make the grid look nice (for formatting purposes)
 
 	for i := 0; i <= 13; i++ {
 		fmt.Printf("|")
@@ -29,8 +37,15 @@ func main() {
 
 	valueInCell := make(map[int]string)
 	valueInCell[1] = "W"
-	valueInCell[10] = "bo"
-	valueInCell[100] = "1111"
+	valueInCell[14] = "bo"
+	valueInCell[183] = "hey"
+	valueInCell[196] = "1111"
 
-	draw14x14GridWithValues(valueInCell)
+	err := draw14x14GridWithValues(valueInCell)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println(valueInCell)
+	color.Cyan(valueInCell[1])
 }
